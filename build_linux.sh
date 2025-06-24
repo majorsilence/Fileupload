cd9e8b9abd16085fa4ff7b05510c125e8aa5753b
Peter Gill <peter.gill@townsuite.com>
2025-06-24 09:36:17 -0230

    build and archive debian package

---
+++ build_linux.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+VERSION=$(cat FileUpload/FileUpload.csproj | grep "<Version>" | sed 's/[^0-9.]*//g')
+
+dotnet restore FileUpload.sln
+dotnet build -c Release FileUpload.sln --no-restore
+dotnet publish -c Release -r linux-x64 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true
+
+rm -rf /build/linux
+mkdir -p build/linux/opt/majorsilence/fileupload
+cp -r FileUpload/bin/Release/net8.0/linux-x64/publish/* build/linux/opt/majorsilence/fileupload/
+chmod +x build/linux/opt/majorsilence/fileupload/FileUpload
+mkdir -p build/linux/usr/bin
+cp -r majorsilence-fileupload build/linux/usr/bin/majorsilence-fileupload
+chmod +x build/linux/usr/bin/majorsilence-fileupload
+
+
+sudo gem install fpm --no-document
+
+cd build/linux
+# build a deb package using fpm
+fpm -s dir -t deb \
+  --name majorsilence-fileupload \
+  --version $VERSION \
+  --description "Majorsilence Fileupload tool.  Interact with azure blob storage, box.com and others as if they are ftp servers." \
+  --maintainer "Majorsilence" \
+  --license "MIT" \
+  --architecture all \
+  --deb-no-default-config-files \
+  --url "https://github.com/majorsilence/Fileupload" \
+  --maintainer "Peter Gill <peter@majorsilence.com>" \
+  ./
+
+  cd ../../
