30a1569066a9e831d315120c90e30faae7e8c670
Peter Gill <peter.gill@townsuite.com>
2026-06-08 16:32:28 -0230

    refactor build scripts to use output directory for published files

--- build_linux.sh
+++ build_linux.sh
@@ -4,12 +4,12 @@ VERSION=$(cat FileUpload/FileUpload.csproj | grep "<Version>" | sed 's/[^0-9.]*/
 
 dotnet restore FileUpload.sln
 dotnet build -c Release FileUpload.sln --no-restore
-dotnet publish -c Release -r linux-x64 --self-contained true
-dotnet publish -c Release -r linux-arm64 --self-contained true
+dotnet publish -c Release -r linux-x64 --self-contained true --output ./build/output/linux-x64
+dotnet publish -c Release -r linux-arm64 --self-contained true --output ./build/output/linux-arm64
 
-rm -rf /build/linux
+rm -rf build/linux
 mkdir -p build/linux/opt/majorsilence/fileupload
-cp -r FileUpload/bin/Release/net8.0/linux-x64/publish/* build/linux/opt/majorsilence/fileupload/
+cp -r build/output/linux-x64/* build/linux/opt/majorsilence/fileupload/
 chmod +x build/linux/opt/majorsilence/fileupload/FileUpload
 mkdir -p build/linux/usr/bin
 cp -r majorsilence-fileupload build/linux/usr/bin/majorsilence-fileupload
